de294d54ddaee634c814bac90fcf554f7dec87a6
Dorota Figas <dfigas13@gmail.com>
2022-03-10 14:53:30 +0100

    darabank class

--- src/com/compay/course/Main.java
+++ src/com/compay/course/Main.java
@@ -3,6 +3,9 @@ package com.compay.course;
 public class Main {
 
     public static void main(String[] args) {
+// project structure => libraries => from maven =>  org.mariadb.jdbc:mariadb-java-client
+
+
 
     }
 }
