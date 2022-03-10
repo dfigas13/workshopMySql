70f9b5dc3e27447a94fd1aead432bdbd441ae4e6
Dorota Figas <dfigas13@gmail.com>
2022-03-10 14:53:21 +0100

    darabank class

---
+++ src/com/compay/course/DbUtility.java
@@ -0,0 +1,18 @@
+package com.compay.course;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtility {
+    private static final String DB_URL =  "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+
+
+}
+
